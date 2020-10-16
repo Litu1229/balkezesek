6f99f121f130871532da397a9d7e1fa9011948ce
Ripperoni <lcsabi9d@gmail.com>
2020-10-16 12:53:37 +0200

    Add project files.

---
+++ balkezesek/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static List<balkezesek> lista = new List<balkezesek>();
+        static int be = 0;
+        static void Beolvas()
+        {
+            StreamReader sr = new StreamReader("balkezesek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new balkezesek(sr.ReadLine()));
+            }
+            sr.Close();
+            foreach (var i in lista)
+            {
+                Console.WriteLine(i.Suly);
+            }
+        }
+
+        static void Harmadik()
+        {
+            Console.WriteLine(lista.Count);
+        }
+
+        static void Negyedik()
+        {
+            foreach (var l in lista)
+            {
+                if (l.Utolso.Contains("1999-10"))
+                {
+                    Console.WriteLine($"{l.Nev}, {Math.Round(l.Magassag*2.54),1:N1}cm");
+                }
+            }
+        }
+
+        static void Otodik()
+        {
+            while (true)
+            {
+                Console.WriteLine("Írj be egy számot 1990 és 1999 között");
+                int be = Convert.ToInt32(Console.ReadLine());
+            if (be >= 1990 && be <= 1999)
+            {
+                Console.WriteLine("Jó számot adott meg");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Rossz számot adtál meg");
+            }
+            }
+        }
+
+        static void Hatodik()
+        {
+            double suly = 0;
+            double db = 0;
+            foreach (var i in lista)
+            {
+                if (be>=int.Parse(i.Elso.Substring(0,4)) || be <= int.Parse(i.Elso.Substring(0, 4)))
+                {
+                    suly = suly + i.Suly;
+                        db++;
+                }
+            }
+            double atlag = suly / db;
+            Console.WriteLine(atlag);
+        }
+        static void Main(string[] args)
+        {
+            //var b = new balkezesek("Jim Abbott","1989-04-08","1997-07-21",200,75);
+            Beolvas();
+            Console.WriteLine("3.feladat");
+            Harmadik();
+            Console.WriteLine("4.feladat");
+            Negyedik();
+            Console.WriteLine("5.feladat");
+            Otodik();
+            Console.WriteLine("6.feladat");
+            Hatodik();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ balkezesek/balkezesek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    internal class balkezesek
+    {
+    public string Nev { get; private set; }
+    public string Elso { get; private set; }
+    public string Utolso { get; private set; }
+    public int Suly { get; private set; }
+    public int Magassag { get; private set; }
+        public balkezesek(string sor)
+        {
+            string[] adat = sor.Split(';');
+            Nev = adat[0];
+            Elso = adat[1];
+            Utolso = adat[2];
+            Suly = Convert.ToInt32(adat[3]);
+            Magassag = Convert.ToInt32(adat[4]);
+        }
+}
+}
